7b373a23cdfb8e3057406619aef93f747a2f037b
andrewm <ajmtwo@gmail.com>
2024-12-27 15:05:22 -0500

    01-01 IsUnique

--- src/main/java/arraystring/_01_01_IsUnique.java
+++ src/main/java/arraystring/_01_01_IsUnique.java
@@ -1,13 +1,23 @@
 package arraystring;
 
+import java.util.HashSet;
+
 /**
  * Implement an algorithm to determine if a string has all unique characters.
  */
 
-class _01_01_IsUnique {
+class _01_01_IsUnique
+{
 
-    boolean isUnique(String str) {
-        throw new UnsupportedOperationException();
+    boolean isUnique(String str)
+    {
+        HashSet<Character> foundChars = new HashSet<>();
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (foundChars.contains(str.charAt(i))) return false;
+            foundChars.add(str.charAt(i));
+        }
+        return true;
     }
 
 }
